b26fedd1610ce893321d6e0c6ed269c86be14f8a
sharmeen466 <sharmeennazeer2@gmail.com>
2025-04-18 00:03:45 +0500

    calculator

---
+++ app.js
@@ -0,0 +1,11 @@
+const calculator = document.getElementById("calculator");
+const numbers = document.getElementsByClassName("boxes")
+const screen = document.getElementsByClassName("screen")
+console.log(screen);
+
+for(i = 0; i < numbers.length; i++){
+    console.log(i);
+    numbers[i].addEventListener("click", function(){
+        
+    })
+}
