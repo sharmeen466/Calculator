a5acc68989a7a3d1ca7adf99e4aeba858a481902
sharmeen466 <sharmeennazeer2@gmail.com>
2025-04-18 03:24:16 +0500

    calculator

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 const calculator = document.getElementById("calculator");
 const numbers = document.getElementsByClassName("boxes")
-const screen = document.getElementsByClassName("screen")
-console.log(screen);
+const screen = document.querySelector(".screen")
 
-for(i = 0; i < numbers.length; i++){
-    console.log(i);
-    numbers[i].addEventListener("click", function(){
-        
+let expression = ""
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", function (event) {
+        let btnValue = event.target.innerText
+
+        if (btnValue === "=") {
+            try {
+                let result = eval(expression)
+                screen.textContent = result
+                expression = "";
+            }
+
+            catch (error) {
+                screen.textContent = "Error"
+                expression = "";
+            }
+        }
+        else if (btnValue === "C") {
+            screen.textContent = "";
+            expression = ""
+        }
+        else if (btnValue === "x") {
+            screen.textContent += "x"
+            expression += "*"
+        }
+
+        else {
+            screen.textContent += btnValue
+            expression += btnValue
+        }
+    })
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", function (event) {
+        for (let j = 0; j < numbers.length; j++) {
+
+            if (numbers[j] !== event.target) {
+                numbers[j].classList.add("inactive")
+                numbers[j].classList.remove("active")
+            }
+
+        }
+        event.target.classList.add("active")
+        event.target.classList.remove("inactive")
     })
 }
